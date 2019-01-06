c6fb2a16255ba09816fb333c693306cabf2b840f
Palash Nigam <npalash25@gmail.com>
2019-01-07 01:26:40 +0530

    Multiple changes
    * Add basic auth header
    * Set request headers
    * Add http.Request to Request struct

--- request.go
+++ request.go
@@ -1,18 +1,64 @@
 package unirest
 
+import (
+	"encoding/base64"
+	"net/http"
+	"strings"
+)
+
+// Request defines the contents of a unirest-request
 type Request struct {
-	method  string
-	url     string
-	headers map[string]interface{}
-	body    map[string]interface{}
+	method      string
+	url         string
+	headers     map[string]interface{}
+	body        interface{}
+	auth        map[string]string
+	HTTPRequest *http.Request
 }
 
-func NewRequest(method string, url string, headers map[string]interface{}, body interface{}, auth [string]string) *Request {
+// NewRequest is a constructor that initializes the Request struct.
+func NewRequest(method, url string, headers map[string]interface{}, body interface{}, auth map[string]string) *Request {
 	uniReq := Request{
-		method:  method,
-		url:     url,
-		headers: headers,
-		body:    body,
+		method:      method,
+		url:         url,
+		headers:     headers,
+		body:        body,
+		auth:        auth,
+		HTTPRequest: nil,
 	}
-	return uniReq
+	return &uniReq
+}
+
+// Do performs the actual HTTP request after encoding the params and setting
+// the required request headers.
+func (r *Request) Do() {
+	// In case of GET request the params are encoded into
+	// url otherwise we use form-encoded params or multipart
+	// form-encoded based on whether the params contain a file
+	// to be uploaded or not.
+	if r.method == "GET" {
+		r.rawEncode()
+		r.HTTPRequest, _ = http.NewRequest("GET", r.url, nil)
+	} else {
+		bodyMap, _ := r.body.(map[string]interface{})
+		for key, value := range bodyMap {
+			if key == "file" {
+				delete(bodyMap, key)
+				r.multiPartFormEncode(key, value.(string), bodyMap)
+			}
+		}
+		r.formEncode()
+	}
+
+	// Setting headers received from the user to the
+	// actual `Header` struct defined in the `net/http` package
+	for k, v := range r.headers {
+		r.HTTPRequest.Header.Set(strings.ToLower(k), v.(string))
+	}
+
+	if r.auth != nil {
+		data := []byte(r.auth["user"] + ":" + r.auth["password"])
+		str := base64.StdEncoding.EncodeToString(data)
+		r.HTTPRequest.Header.Set("Authorization", "Basic"+str)
+	}
 }
