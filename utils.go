42546c92adef3c6bff4b9d27e2197740ccf15d4b
Palash Nigam <npalash25@gmail.com>
2019-01-06 21:42:21 +0530

    Add multipart form encoding

--- utils.go
+++ utils.go
@@ -1,41 +1,88 @@
 package unirest
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"mime/multipart"
+	"net/http"
 	"net/url"
+	"os"
+	"path/filepath"
 	"reflect"
 	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pborman/uuid"
 )
 
-func (r *Request) rawEncode() *Request {
-	var valType
-	var _url *neturl.URL
-	_url = neturl.Parse(r.url)
+func iterateParams(params map[string]interface{}) url.Values {
+	var valType string
+	values := url.Values{}
 
-	values = neturl.Values{}
-	for k, v := range r.body {
+	for k, v := range params {
 		valType = reflect.TypeOf(v).Kind().String()
 		if valType != "string" {
-			v = toString(v, valType)
-			for _, str := range v {
-				values.Add(k, v)
-			}
+			str := toString(reflect.ValueOf(v), valType)
+			values.Add(k, str)
 		} else {
-			values.Add(k, v)
+			values.Add(k, v.(string))
 		}
 	}
+	return values
+}
+
+func (r *Request) rawEncode() string {
+	var _url *url.URL
+	_url, _ = url.Parse(r.url)
+
+	values := iterateParams(r.body.(map[string]interface{}))
 	_url.RawQuery = values.Encode()
+
 	return _url.String()
 }
 
-func (r *Request) formEncode() *Request {
-	// add form encode header
+func (r *Request) formEncode() *strings.Reader {
+	form := iterateParams(r.body.(map[string]interface{}))
+	r.headers.Set("Content-Type", "application/x-www-form-urlencoded")
+	return strings.NewReader(form.Encode())
 }
 
-func (r *Request) multiPartFormEncode() *Request {
-	// add multipart header
+func (r *Request) multiPartFormEncode(paramName, path string, params map[string]interface{}) (*Request, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile(paramName, filepath.Base(path))
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.Copy(part, file)
+
+	for key, val := range params {
+		_ = writer.WriteField(key, val.(string))
+	}
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", r.url, body)
+	if err != nil {
+		return nil, err
+	}
+	r.HTTPRequest = req
+	r.headers.Set("Content-Type", writer.FormDataContentType())
+
+	return r, nil
 }
 
-func toString(value interface{}, valType string) []string {
+func toString(value reflect.Value, valType string) string {
 	switch valType {
 	case "bool":
 		return strconv.FormatBool(value.Bool())
