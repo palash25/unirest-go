26303259027b68b199a4b649b46a578680448d4f
Palash Nigam <npalash25@gmail.com>
2019-02-02 18:14:04 +0530

    Add async mode

--- unirest.go
+++ unirest.go
@@ -1,64 +1,108 @@
 package unirest
 
+import (
+	"net/http"
+	"sync"
+)
+
 // UnirestClient defines the basic options for a client
 type UnirestClient struct {
-	timeout        int
-	userAgent      string
-	defaultHeaders map[string]string
+	Timeout        int
+	UserAgent      string
+	DefaultHeaders map[string]string
+	InChan         chan *Request
+	OutChan        chan *AsyncRequest
 }
 
 // NewClient is a constructor that initializes unirest client.
 func NewClient() *UnirestClient {
 	return &UnirestClient{
-		timeout:        10,
-		userAgent:      "unirest-go/1.0",
-		defaultHeaders: make(map[string]string),
+		Timeout:        10,
+		UserAgent:      "unirest-go/1.0",
+		DefaultHeaders: make(map[string]string),
+		InChan:         make(chan *Request),
+		OutChan:        make(chan *AsyncRequest),
 	}
 }
 
 // Get makes get requests using the UnirestClient
-func (c *UnirestClient) Get(url string, headers map[string]interface{}, body interface{}, auth map[string]string) (*Response, error) {
+func (uc *UnirestClient) Get(url string, headers map[string]interface{}, body interface{}, auth map[string]string) *Request {
 	r := NewRequest("GET", url, headers, body, auth)
-	r.MakeRequest()
-	return nil, nil
+	return r.BuildRequest(uc)
 }
 
-// Get makes get requests using the UnirestClient
-func (c *UnirestClient) Post(url string, headers map[string]interface{}, body interface{}, auth map[string]string) (*Response, error) {
+// Post makes get requests using the UnirestClient
+func (uc *UnirestClient) Post(url string, headers map[string]interface{}, body interface{}, auth map[string]string) *Request {
 	r := NewRequest("POST", url, headers, body, auth)
-	r.MakeRequest()
-	return nil, nil
+	return r.BuildRequest(uc)
 }
 
-// Get makes get requests using the UnirestClient
-func (c *UnirestClient) Put(url string, headers map[string]interface{}, body interface{}, auth map[string]string) (*Response, error) {
+// Put makes get requests using the UnirestClient
+func (uc *UnirestClient) Put(url string, headers map[string]interface{}, body interface{}, auth map[string]string) *Request {
 	r := NewRequest("PUT", url, headers, body, auth)
-	r.MakeRequest()
-	return nil, nil
+	return r.BuildRequest(uc)
 }
 
-// Get makes get requests using the UnirestClient
-func (c *UnirestClient) Delete(url string, headers map[string]interface{}, body interface{}, auth map[string]string) (*Response, error) {
+// Delete makes get requests using the UnirestClient
+func (uc *UnirestClient) Delete(url string, headers map[string]interface{}, body interface{}, auth map[string]string) *Request {
 	r := NewRequest("DELETE", url, headers, body, auth)
-	r.MakeRequest()
-	return nil, nil
+	return r.BuildRequest(uc)
 }
 
-// Get makes get requests using the UnirestClient
-func (c *UnirestClient) Patch(url string, headers map[string]interface{}, body interface{}, auth map[string]string) (*Response, error) {
+// Patch makes get requests using the UnirestClient
+func (uc *UnirestClient) Patch(url string, headers map[string]interface{}, body interface{}, auth map[string]string) *Request {
 	r := NewRequest("PATCH", url, headers, body, auth)
-	r.MakeRequest()
-	return nil, nil
+	return r.BuildRequest(uc)
 }
 
-func (c *UnirestClient) SetTimeout(timeout int) {
-	c.timeout = timeout
+func (uc *UnirestClient) SetTimeout(timeout int) {
+	uc.Timeout = timeout
 }
 
-func (c *UnirestClient) SetUserAgent(agent string) {
-	c.userAgent = agent
+func (uc *UnirestClient) SetUserAgent(agent string) {
+	if agent != "" {
+		uc.UserAgent = agent
+	}
 }
 
-func (c *UnirestClient) SetDefaultHeader(key, value string) {
-	c.defaultHeaders[key] = value
+func (uc *UnirestClient) SetDefaultHeader(key, value string) {
+	uc.DefaultHeaders[key] = value
+}
+
+func (uc *UnirestClient) ClearDefaultHeader() {
+	uc.DefaultHeaders = make(map[string]string)
+}
+
+// Do performs synchronous requests
+func (r *Request) Do() (*http.Response, error) {
+	resp, err := r.HTTPClient.Do(r.HTTPRequest)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+// DoAsync performs requests asynchronously that it receives from the input
+// channel and sends the response or errors to the output channel
+func (uc *UnirestClient) DoAsync() {
+	wg := sync.WaitGroup{}
+
+	for req := range uc.InChan {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			res, err := req.HTTPClient.Do(req.HTTPRequest)
+			//resp := NewResponse(res)
+			asyncResp := &AsyncRequest{
+				Resp: res,
+				Err:  err,
+			}
+			uc.OutChan <- asyncResp
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(uc.OutChan)
+	}()
 }
